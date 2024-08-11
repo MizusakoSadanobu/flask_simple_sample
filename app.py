ac225bd7cd17b8529dca4244bf1e2c2590d27679
sadanobu.mizusako <sadanobu.mizusako@gmail.com>
2024-08-11 12:37:23 +0900

    DB migration

--- app.py
+++ app.py
@@ -1,38 +1,52 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 # Flaskアプリケーションのインスタンスを作成
 app = Flask(__name__)
 
-# 仮のデータベースとして、タスクを格納するリストを用意
-tasks = []
+# SQLiteデータベースの設定
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# ルートURLにアクセスした際の処理
+# SQLAlchemyインスタンスを作成
+db = SQLAlchemy(app)
+
+# タスクのデータベースモデルを定義
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<Task {self.id}: {self.content}>'
+
+# 初回実行時にデータベースを作成
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def index():
-    # `index.html` テンプレートをレンダリングし、`tasks` リストを渡す
+    # データベースから全てのタスクを取得
+    tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
 
-# 新しいタスクを追加するためのルート
 @app.route('/add', methods=['POST'])
 def add_task():
     # フォームから送信されたタスクを取得
-    task = request.form.get('task')
-    # タスクが空でない場合はリストに追加
-    if task:
-        tasks.append(task)
-    # メインページにリダイレクト
+    task_content = request.form.get('task')
+    if task_content:
+        # 新しいタスクをデータベースに追加
+        new_task = Task(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
     return redirect(url_for('index'))
 
-# タスクを削除するためのルート
 @app.route('/delete/<int:task_id>')
 def delete_task(task_id):
-    # タスクIDがリストの範囲内であれば、該当タスクを削除
-    if 0 <= task_id < len(tasks):
-        tasks.pop(task_id)
-    # メインページにリダイレクト
+    # 指定されたIDのタスクをデータベースから削除
+    task_to_delete = Task.query.get_or_404(task_id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
     return redirect(url_for('index'))
 
-# アプリケーションのエントリーポイント
 if __name__ == '__main__':
-    # デバッグモードでアプリケーションを起動
     app.run(debug=True)
